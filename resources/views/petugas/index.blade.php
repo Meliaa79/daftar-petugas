d466de65d57cfc6dedb45cffa1e4ae4683286f27
Meliaa79 <146916708+Meliaa79@users.noreply.github.com>
2024-12-20 09:36:24 +0800

    Update index.blade.php

--- resources/views/petugas/index.blade.php
+++ resources/views/petugas/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Petugas</title>
+    <title>Daftar Petugas Provinsi Bali</title>
     @vite('resources/css/app.css') <!-- Menghubungkan CSS dengan Tailwind -->
 </head>
 <body>
